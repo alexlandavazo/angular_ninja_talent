a27f60592f119aa6ea51bbff13bedc3b651a1adf
Alejandro Landavazo <alejandrolandavazo@gmail.com>
2021-03-10 11:02:33 -0700

    Deleting client

--- jest.config.js
+++ jest.config.js
@@ -6,7 +6,6 @@ module.exports = {
     '<rootDir>/libs/core-data',
     '<rootDir>/libs/core-state',
     '<rootDir>/libs/material',
-    '<rootDir>/apps/client',
     '<rootDir>/libs/ui-toolbar',
   ],
 };
